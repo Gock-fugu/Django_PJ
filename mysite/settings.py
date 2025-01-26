526f4ec912f5b5ef6fd72b288b67036e6a2dc802
Gock-fugu <sdilnij@gmail.com>
2025-01-26 19:45:54 +0200

    MY MESSAGE HERE

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@9jzsfil%z427v(_67ye))_p$h_j^h&mh#pvry%fqr*le8ox#h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
