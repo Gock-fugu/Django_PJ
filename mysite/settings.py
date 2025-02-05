be2c6fde7e66629d6e42847e319e576585fc606e
Gock-fugu <99351815+Gock-fugu@users.noreply.github.com>
2025-02-05 22:09:06 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600)
 }
 
 
