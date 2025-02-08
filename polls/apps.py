13a1eb98494a7a5b76059ece289be1d4f50795a2
Gock-fugu <99351815+Gock-fugu@users.noreply.github.com>
2025-02-08 09:38:22 +0200

    Update apps.py

--- polls/apps.py
+++ polls/apps.py
@@ -1,15 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 
-class MyAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "polls"
 
-    def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username="admin",
-                email="sdilnij@gmail.com",
-                password="239c2c0ffd88d0ebf290247267b4ecfd3"
-            )
+class PollsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'polls'
