c2c271fc5bd564289d36c6f23ea15b726e1042b3
Gock-fugu <99351815+Gock-fugu@users.noreply.github.com>
2025-02-08 09:39:01 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ['mysite-6n1f.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.MyAppConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
